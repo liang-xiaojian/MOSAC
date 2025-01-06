69f9b547dd2811ab9abea946b479e49d634527f2
liang-xiaojian <56351491+liang-xiaojian@users.noreply.github.com>
2025-01-06 13:07:12 +0800

    Update true_cr.cc

--- mosac/cr/true_cr.cc
+++ mosac/cr/true_cr.cc
@@ -9,24 +9,92 @@
 namespace mosac {
 
 namespace {
-const std::map<size_t, size_t> kExtend = {
-    {1 << 4, 9},  {1 << 5, 8},  {1 << 6, 7},  {1 << 7, 6},  {1 << 8, 6},
-    {1 << 9, 5},  {1 << 10, 5}, {1 << 11, 5}, {1 << 12, 4}, {1 << 13, 4},
-    {1 << 14, 4}, {1 << 15, 4}, {1 << 16, 4}, {1 << 17, 4}, {1 << 18, 4},
-    {1 << 19, 3}, {1 << 20, 3}};
 
-size_t findB(size_t num) {
+const std::map<size_t, size_t> kExtend4 = {
+    {1 << 4, 19},  {1 << 5, 17},  {1 << 6, 16},  {1 << 7, 16},  {1 << 8, 15},
+    {1 << 9, 14},  {1 << 10, 14}, {1 << 11, 14}, {1 << 12, 14}, {1 << 13, 13},
+    {1 << 14, 13}, {1 << 15, 13}, {1 << 16, 13}, {1 << 17, 13}, {1 << 18, 12},
+    {1 << 19, 12}, {1 << 20, 12}};
+
+const std::map<size_t, size_t> kExtend5 = {
+    {1 << 4, 17},  {1 << 5, 15},  {1 << 6, 14},  {1 << 7, 14},  {1 << 8, 14},
+    {1 << 9, 13},  {1 << 10, 13}, {1 << 11, 12}, {1 << 12, 12}, {1 << 13, 11},
+    {1 << 14, 11}, {1 << 15, 11}, {1 << 16, 11}, {1 << 17, 11}, {1 << 18, 11},
+    {1 << 19, 10}, {1 << 20, 10}};
+
+const std::map<size_t, size_t> kExtend6 = {
+    {1 << 4, 16},  {1 << 5, 14},  {1 << 6, 13},  {1 << 7, 13},  {1 << 8, 12},
+    {1 << 9, 12},  {1 << 10, 11}, {1 << 11, 11}, {1 << 12, 11}, {1 << 13, 10},
+    {1 << 14, 10}, {1 << 15, 10}, {1 << 16, 10}, {1 << 17, 10}, {1 << 18, 10},
+    {1 << 19, 9},  {1 << 20, 9}};
+
+const std::map<size_t, size_t> kExtend7 = {
+    {1 << 4, 15}, {1 << 5, 13},  {1 << 6, 12},  {1 << 7, 12},  {1 << 8, 11},
+    {1 << 9, 11}, {1 << 10, 10}, {1 << 11, 10}, {1 << 12, 10}, {1 << 13, 9},
+    {1 << 14, 9}, {1 << 15, 9},  {1 << 16, 9},  {1 << 17, 9},  {1 << 18, 9},
+    {1 << 19, 8}, {1 << 20, 8}};
+
+const std::map<size_t, size_t> kExtend8 = {
+    {1 << 4, 14}, {1 << 5, 12}, {1 << 6, 11}, {1 << 7, 11}, {1 << 8, 10},
+    {1 << 9, 10}, {1 << 10, 9}, {1 << 11, 9}, {1 << 12, 9}, {1 << 13, 8},
+    {1 << 14, 8}, {1 << 15, 8}, {1 << 16, 8}, {1 << 17, 8}, {1 << 18, 8},
+    {1 << 19, 7}, {1 << 20, 7}};
+
+const std::map<size_t, size_t> kExtend9 = {
+    {1 << 4, 14}, {1 << 5, 12}, {1 << 6, 11}, {1 << 7, 11}, {1 << 8, 10},
+    {1 << 9, 10}, {1 << 10, 9}, {1 << 11, 9}, {1 << 12, 9}, {1 << 13, 8},
+    {1 << 14, 8}, {1 << 15, 8}, {1 << 16, 8}, {1 << 17, 8}, {1 << 18, 8},
+    {1 << 19, 7}, {1 << 20, 7}};
+
+const std::map<size_t, size_t> kExtend10 = {
+    {1 << 4, 12}, {1 << 5, 11}, {1 << 6, 10}, {1 << 7, 9},  {1 << 8, 9},
+    {1 << 9, 8},  {1 << 10, 8}, {1 << 11, 8}, {1 << 12, 8}, {1 << 13, 7},
+    {1 << 14, 7}, {1 << 15, 7}, {1 << 16, 7}, {1 << 17, 7}, {1 << 18, 7},
+    {1 << 19, 6}, {1 << 20, 6}};
+
+size_t ASTFindB(const std::map<size_t, size_t>& mapping, size_t num) {
   const size_t kMagicSize = 1 << 12;
   if (num <= kMagicSize) {
-    return kExtend.at(kMagicSize);
+    return mapping.at(kMagicSize);
   }
 
-  auto it = kExtend.lower_bound(num);
-  if (it == kExtend.end()) {
+  auto it = mapping.lower_bound(num);
+  if (it == mapping.end()) {
     SPDLOG_INFO("[Warning] num is too large");
-    return kExtend.crbegin()->second;
+    return mapping.crbegin()->second;
   }
   return it->second;
+}
+
+size_t findB(size_t T, size_t num) {
+  auto logT = yacl::math::Log2Ceil(T);
+
+  if (logT <= 4) {
+    return ASTFindB(kExtend4, num);
+  }
+
+  size_t B = 1;
+  switch (logT) {
+    case 5:
+      B = ASTFindB(kExtend5, num);
+      break;
+    case 6:
+      B = ASTFindB(kExtend6, num);
+      break;
+    case 7:
+      B = ASTFindB(kExtend7, num);
+      break;
+    case 8:
+      B = ASTFindB(kExtend8, num);
+      break;
+    case 9:
+      B = ASTFindB(kExtend9, num);
+      break;
+    default:
+      B = ASTFindB(kExtend10, num);
+      break;
+  }
+  return B;
 }
 
 }  // namespace
@@ -908,7 +976,7 @@ std::vector<size_t> TrueCorrelation::ASTSet_2k(size_t T,
 
   size_t total = T_num * depth;
 
-  const auto ot_per_ast = yacl::math::Log2Ceil(T) * T * findB(total);
+  const auto ot_per_ast = yacl::math::Log2Ceil(T) * T * findB(T, total);
   const auto batch_size = yacl::math::DivCeil(param::kBatchOtSize, ot_per_ast);
   size_t batch_num = yacl::math::DivCeil(total, batch_size);
 
@@ -1031,7 +1099,7 @@ void TrueCorrelation::ASTGet_2k(size_t T, absl::Span<internal::ATy> a,
   std::vector<std::vector<internal::ATy>> vec_b_T_all;
 
   size_t total = T_num * depth;
-  const auto ot_per_ast = yacl::math::Log2Ceil(T) * T * findB(total);
+  const auto ot_per_ast = yacl::math::Log2Ceil(T) * T * findB(T, total);
   const auto batch_size = yacl::math::DivCeil(param::kBatchOtSize, ot_per_ast);
   size_t batch_num = yacl::math::DivCeil(total, batch_size);
 
@@ -1136,7 +1204,7 @@ std::vector<std::vector<size_t>> TrueCorrelation::ASTSet_batch_basic_2k(
   // YACL_ENFORCE((num & (num - 1)) == 0);
   YACL_ENFORCE((T & (T - 1)) == 0);
 
-  auto ext = findB(num);  // NB choose and cut
+  auto ext = findB(T, num);  // NB choose and cut
   auto ext_num = num * ext;
 
   SPDLOG_INFO("T is {}, num is {}, num extend is {}", T, num, ext_num);
@@ -1184,7 +1252,7 @@ void TrueCorrelation::ASTGet_batch_basic_2k(
   // YACL_ENFORCE((num & (num - 1)) == 0);
   YACL_ENFORCE((T & (T - 1)) == 0);
 
-  auto ext = findB(num);  // NB choose and cut
+  auto ext = findB(T, num);  // NB choose and cut
   auto ext_num = num * ext;
 
   SPDLOG_INFO("T is {}, num is {}, num extend is {}", T, num, ext_num);
